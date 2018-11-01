36a308966dd30028af94e98abacf3dcfb5abbfab
TOFarmer <33081141+TOFarmer@users.noreply.github.com>
2018-11-01 14:04:59 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="MD_property_analysis",
+    version="0.1",
+    packages=find_packages(),
+    author="Thomas Farmer",
+    url="https://github.com/TOFarmer/MD_property_analysis",
+    install_requires=["numpy","matplotlib"]
+)
